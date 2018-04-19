e2a4505842ce04c3383b65baacd20b73ff2e50ab
maslowski.it@gmail.com <CoolerMaster430>
2018-04-19 10:05:20 +0200

    Small changes in js

--- user/sendContactEmail/js/sendEmail.js
+++ user/sendContactEmail/js/sendEmail.js
@@ -1,53 +1,46 @@
 $(document).ready(function(){
-    $("#checkform").submit(function() { // loginForm is submitted
+    $("#checkform").submit(function() { // checkForm is submitted
         var mail=new Object();
         mail.email=$('#email').val();//get values
         mail.name= $('#subject').val();  
 		mail.text= $('#message').val();  		
         event.preventDefault();
         
-        if(mail.email && mail.name){ //check if fields are filled
+        if(mail.email && mail.name && mail.text){ //check if fields are filled
            post_data(mail)
         }else{
-            loginEmptyFields();
+            EmptyFields();
         }
     });
   });
 
-  function loginAccepted(data){
-    var dataObject=JSON.parse(data.responseText);    
-    localStorage.setItem("token",dataObject.token); //save token in local memory
-
-    if(dataObject.user.authorities.length>1)
-    window.location.href="../../Reviewer/mainPageReviewer/index.html";
-    
-    if(dataObject.user.authorities[0].role==="AUTHOR")
-    window.location.href="../../Author/mainPageAuthor/index.html";
-    else
-    window.location.href="../../Admin/mainPageAdmin/index.html";
+  function Accepted(data){
+	      $('#message').html('Email został wysłany').css("color","#6AA730");
+    $('#message-box').css("display","block").css("background","#C5F9A7").css("border","1px solid #6AA730");
   }
 
-  function loginRejected(){
-    $('#errMessage').html( " Nieprawidłowo podany email lub hasło");
+  function Rejected(){
+    $('#errMessage').html( "Wystąpił błąd! Spróbuj ponownie za chwilę!");
     $('#message-box').css("display","block");
     }
 
-    function loginEmptyFields(){
-        $('#errMessage').html( "Uzupełnij wszystkie pola");
-        $('#message-box').css("display","block");
+    function EmptyFields(){
+        $('#message').html('Wypełnij pole Email').css("color","#D52727");
+        $('#message-box').css("display","block").css("background","#FFEBE8")
+        .css("border","1px solid #D52727");    
     }
 
-    function post_data(user){
+    function post_data(mail){
         $.ajax({
             type: "POST",
             headers : {
                 "content-type" : "application/json"
             },
             url: `${SERVER_URL}/user/sendContactEmail`,
-            data: JSON.stringify(user),
+            data: JSON.stringify(mail),
             complete: function(data) {
-                if(data.status===200) loginAccepted(data); //check response status
-                else loginRejected();
+                if(data.status===200) Accepted(data); //check response status
+                else Rejected();
             }  ,
             dataType: 'application/json'
         });
